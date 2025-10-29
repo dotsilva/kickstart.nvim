b2e5114a6f4b446e85e1e6f263d25a5aa6a1a640
dotsilva <devbrunodasilva@gmail.com>
2025-10-29 13:53:42 -0300

    hm

---
+++ lua/custom/plugins/ksui.lua
@@ -0,0 +1,62 @@
+return {
+  -- 2. DRESSING (from pcdressfix.lua)
+  {
+    'stevearc/dressing.nvim',
+    event = 'VeryLazy', -- default for kickstart ui plugins
+  },
+
+  -- 3. SNACKS (Corrected as per :checkhealth)
+  {
+    'folke/snacks.nvim',
+    lazy = false, -- <--- CORREÇÃO 1: (was 'event = VeryLazy')
+    priority = 1000, -- <--- CORREÇÃO 2: Added priority
+    opts = {
+      scroll = {
+        enabled = false, -- Disable scrolling animations
+      },
+      -- NOTA: O 'dashboard' do snacks não está habilitado.
+      -- Isso permite que o alpha-nvim funcione sem conflito.
+    },
+  },
+
+  -- alpha-nvim dashboard (Restored)
+  {
+    'goolord/alpha-nvim',
+    lazy = false,
+    config = function()
+      local alpha = require 'alpha'
+      local dashboard = require 'alpha.themes.dashboard'
+
+      dashboard.section.header.val = vim.split(
+        [[
+                                   .-----.          
+        .----------------------.   | === |          
+        |.-""""""""""""""""""-.|   |-----|          
+        ||                    ||   | === |          
+        ||   KICKSTART.NVIM   ||   |-----|          
+        ||                    ||   | === |          
+        ||                    ||   |-----|          
+        ||:Tutor              ||   |:::::|          
+        |'-..................-'|   |____o|          
+        `"")----------------(""`   ___________      
+       /::::::::::|  |::::::::::\  \ no mouse \     
+      /:::========|  |==hjkl==:::\  \ required \    
+     '""""""""""""'  '""""""""""""'  '""""""""""'   
+]],
+        '\n'
+      )
+
+      dashboard.section.buttons.val = {
+        dashboard.button('e', 'New file', ':ene <BAR> startinsert <CR>'),
+        dashboard.button('r', 'Recent files', ':Telescope oldfiles <CR>'),
+        dashboard.button('q', 'Quit Neovim', ':qa<CR>'),
+      }
+
+      dashboard.section.header.opts.hl = 'Type'
+      dashboard.section.buttons.opts.hl = 'Function'
+      dashboard.opts.layout[1].val = 2
+
+      alpha.setup(dashboard.config)
+    end,
+  },
+}
