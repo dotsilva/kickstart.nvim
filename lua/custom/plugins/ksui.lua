4e1fa6787c2beaa934b860fadafc68aae2b3a8ed
dotsilva <devbrunodasilva@gmail.com>
2025-10-29 15:48:39 -0300

    fixing identscope in alpha

--- lua/custom/plugins/ksui.lua
+++ lua/custom/plugins/ksui.lua
@@ -16,15 +16,29 @@ return {
       },
     },
   },
+
   -- vertical line
   {
     'echasnovski/mini.indentscope',
     version = false, -- or it might have a version tag
 
-    -- This function disables the plugin for the listed filetypes/buftypes
-    enabled = function()
-      -- Set of filetypes to disable
+    -- Plugin options
+    opts = {
+      -- You can uncomment and change the symbol if you want
+      symbol = '│',
+      options = { try_as_border = true },
+      -- Add this line to disable animation:
+      draw = { animation = require('mini.indentscope').gen_animation.none() },
+    },
+
+    -- This config function runs after the plugin is loaded
+    config = function(_, opts)
+      -- 1. Setup the plugin with the options defined above
+      require('mini.indentscope').setup(opts)
+
+      -- 2. Define the filetypes and buftypes to disable
       local disabled_fts = {
+        alpha = true, -- This is the 'alpha-nvim' dashboard
         help = true,
         dashboard = true,
         lazy = true,
@@ -33,28 +47,33 @@ return {
         Trouble = true,
       }
 
-      -- Set of buftypes to disable
       local disabled_bts = {
         terminal = true,
         nofile = true,
       }
 
-      -- If the current filetype or buftype is in the list, return 'false' (disabled)
-      if disabled_fts[vim.bo.filetype] or disabled_bts[vim.bo.buftype] then
-        return false
-      end
+      -- 3. Create an autocommand to disable the plugin for those types
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = '*', -- Check all filetypes
+        group = vim.api.nvim_create_augroup('MiniIndentScopeDisable', { clear = true }),
+        callback = function()
+          -- If the filetype is in our list, disable indent lines for this buffer
+          if disabled_fts[vim.bo.filetype] then
+            vim.b.miniindentscope_disable = true
+          end
+        end,
+      })
 
-      -- Otherwise, return 'true' (enabled)
-      return true
+      vim.api.nvim_create_autocmd('BufEnter', {
+        pattern = '*', -- Check all buffers
+        group = 'MiniIndentScopeDisable', -- Use the same group
+        callback = function()
+          -- If the buftype is in our list, disable indent lines
+          if disabled_bts[vim.bo.buftype] then
+            vim.b.miniindentscope_disable = true
+          end
+        end,
+      })
     end,
-
-    -- Options for the plugin
-    opts = {
-      -- You can uncomment and change the symbol if you want
-      symbol = '│',
-      options = { try_as_border = true },
-      -- Add this line to disable animation:
-      draw = { animation = require('mini.indentscope').gen_animation.none() },
-    },
   },
 }
