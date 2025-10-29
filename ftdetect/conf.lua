4075028ab31b5f19fbacc25bdccc49b910fe688c
dotsilva <devbrunodasilva@gmail.com>
2025-10-29 09:54:08 -0300

    improving hyprland detection

--- ftdetect/conf.lua
+++ ftdetect/conf.lua
@@ -1,33 +1,29 @@
 -- ~/.config/nvim-kickstart/ftdetect/conf.lua
 --
--- Define associações de filetype para arquivos .conf
+-- Define filetype associations for .conf files
 
--- 1. Regra Geral: Todo .conf é 'ini' por padrão
+-- 1. General Rule: All .conf files are 'ini' by default
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = { '*.conf' },
   command = 'set filetype=ini',
 })
 
--- 2. Regra Específica: Arquivos conhecidos do Hyprland são 'hyprlang'
---    (Isso vai sobrescrever a regra geral acima para estes arquivos)
+-- 2. Specific Rule: Hyprland files are 'hyprlang'
+--    (This will override the general rule above for these files)
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = {
-    -- Arquivos da sua lista
+    -- Pattern 1: Any .conf inside a 'hypr' directory
+    '*/hypr/*.conf',
+
+    -- Pattern 2: Official filenames from the Hyprland ecosystem
     'hyprland.conf',
     'hypridle.conf',
     'hyprlock.conf',
-    'bindings.conf',
-    'autostart.conf',
-    'envs.conf',
-    'hyprsunset.conf',
-    'input.conf',
-    'looknfeel.conf',
-    'monitors.conf',
-
-    -- Outros arquivos comuns (para o futuro)
     'hyprpaper.conf',
     'hyprtheme.conf',
-    'windowrules.conf',
+    'hyprsunset.conf',
+
+    -- Pattern 3: Official extension
     '*.hl',
   },
   command = 'set filetype=hyprlang',
