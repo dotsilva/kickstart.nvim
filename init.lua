a70e075f6a637f3d5c4f9e7e31f4bbd51b509fff
Bruno da Silva <dotsilva@proton.me>
2026-03-20 08:03:28 -0300

    upgrade

--- init.lua
+++ init.lua
@@ -805,27 +805,27 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
+  -- { -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  -- 'folke/tokyonight.nvim',
+  -- priority = 1000, -- Make sure to load this before all the other start plugins.
+  -- config = function()
+  --  ---@diagnostic disable-next-line: missing-fields
+  --  require('tokyonight').setup {
+  --    styles = {
+  --      comments = { italic = false }, -- Disable italics in comments
+  --    },
+  --  }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
+  -- Load the colorscheme here.
+  -- Like many other themes, this one has different styles, and you could load
+  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --  vim.cmd.colorscheme 'tokyonight-night'
+  -- end,
+  --  },
 
   -- Highlight todo, notes, etc in comments
   {
@@ -867,8 +867,7 @@ require('lazy').setup({
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function() return '%2l:%-2v' end
-
+      statusline.section_location = function() return string.format('%%2l:%%-2v | chars: %d', vim.fn.wordcount().chars) end
       -- ... and there is more!
       --  Check out: https://github.com/nvim-mini/mini.nvim
     end,
