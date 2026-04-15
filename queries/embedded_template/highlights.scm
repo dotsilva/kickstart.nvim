6f2d1fd68c1870e63ac26f4dce4591613eedf516
Bruno da Silva <dotsilva@proton.me>
2026-04-15 02:08:37 -0300

    erb finished

--- queries/embedded_template/highlights.scm
+++ queries/embedded_template/highlights.scm
@@ -1,17 +1,20 @@
-;; queries/embedded_template/highlights.scm
-;; extends
-
 ;; YELLOW: Routing & Logic
 [
-  "<%#"
   "<%"
   "<%="
+  "<%=="
+  "<%%"
   "<%_"
   "<%-"
   "%>"
   "-%>"
   "_%>"
+  "%%>"
 ] @keyword.conditional
 
 ;; BLACK: Comments
 (comment_directive) @comment @spell
+
+(comment_directive
+  "%>" @comment
+  (#set! priority 110))
