43c6604fede577ffd791eb3527f8669590aab5cd
Bruno da Silva <dotsilva@proton.me>
2026-04-16 19:53:00 -0300

    new filetypes mapped

--- filetype.lua
+++ filetype.lua
@@ -1,27 +1,8 @@
--- PATH/nvim/filetype.lua
-
 vim.filetype.add {
   extension = {
     theme = 'dosini',
+    gitcommit = 'gitcommit',
+    gitconfig = 'gitconfig',
+    gitrebase = 'gitrebase',
   },
-  -- Add any future custom extensions or patterns here
 }
-
--- this is the pattern
---[=[
-vim.filetype.add({
-  extension = {
-    -- example: map .foo files to the 'lua' filetype
-    foo = 'lua',
-    -- your custom files here
-  },
-  filename = {
-    -- example: specific filenames
-    ['.env.local'] = 'sh',
-  },
-  pattern = {
-    -- example: regex patterns
-    ['.*%.env%.%w+'] = 'sh',
-  },
-})
-]=]
