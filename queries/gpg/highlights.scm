c0000b8b9cb5cae1429c9831e37741d23d2514f9
Bruno da Silva <dotsilva@proton.me>
2026-04-16 19:50:27 -0300

    fine tuning gpg parser colors

--- queries/gpg/highlights.scm
+++ queries/gpg/highlights.scm
@@ -1,42 +1,9 @@
 ;; queries/gpg/highlights.scm
 ;; extends
 
-;; YELLOW: Routing & Logic
-[
-  (filter_op0)
-  (filter_op1)
-  (filter_lc)
-] @keyword.conditional
-
-"!" @keyword.conditional
-(option "no-" @keyword.conditional)
-
-;; BLUE: Structure & Definition
-(option
-  .
-  _ @keyword.function)
-
-"sensitive:" @keyword.modifier
-"=" @keyword.modifier
-
-(filter_scope) @module
-
-[
-  "\""
-  "'"
-  ","
-] @punctuation.delimiter
-
-;; CYAN: Ephemeral State
-(option
-  (name) @variable.parameter)
-
-(filter_name) @variable.parameter
-(filter_property) @property
-(key) @variable.builtin
-
-;; MAGENTA: Exceptional Data
-(format) @character.special
+;; ==========================================
+;; GENERIC FALLBACKS 
+;; ==========================================
 
 ;; WHITE: Generic Data
 (string
@@ -47,14 +14,61 @@
   "clear"
 ] @string
 
-(url) @string
 (filter_value) @string
 
+;; BLUE: Structure & Definition
+(option
+  .
+  _ @keyword.function)
+
+"sensitive:" @keyword.modifier
+"=" @punctuation.delimiter
+
+(filter_scope) @module
+
+[
+  "\""
+  "'"
+  ","
+] @punctuation.delimiter
+
+;; ==========================================
+;; SPECIFIC OVERRIDES 
+;; ==========================================
+
+;; WHITE OVERRIDES (Protected Values)
 [
   (number)
   (expire_time)
   (iso_time)
 ] @number
+
+;; YELLOW: Routing & Logic
+[
+  (filter_op0)
+  (filter_op1)
+  (filter_lc)
+] @keyword.conditional
+
+"!" @keyword.conditional
+(option "no-" @keyword.conditional)
+
+;; CYAN: Ephemeral State & References
+(option
+  (name) @variable.parameter)
+
+(filter_name) @variable.parameter
+(filter_property) @property
+(key) @variable.builtin
+
+(url) @property
+
+((string
+  (content) @property)
+  (#lua-match? @property "^[~./]"))
+
+;; MAGENTA: Exceptional Data
+(format) @character.special
 
 ;; BLACK: Comments
 (comment) @comment @spell
