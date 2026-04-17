a70b32dc4e276bd3382b288114d299e5ff0ddd27
Bruno da Silva <dotsilva@proton.me>
2026-04-16 21:43:54 -0300

    http parser done

--- queries/http/highlights.scm
+++ queries/http/highlights.scm
@@ -1,82 +1,52 @@
-;; extends
-
 ;; RED: Halts, Exceptions & Deletions
 ((method) @keyword.exception
-  (#any-of? @keyword.exception "DELETE"))
+  (#eq? @keyword.exception "DELETE"))
 
 ;; GREEN: Triggers & Mutations
 ((method) @function.builtin
-  (#any-of? @function.builtin "POST" "PUT" "PATCH"))
+  (#any-of? @function.builtin "POST" "PUT" "PATCH" "GRAPHQL"))
+
+;; CYAN: Ephemeral State & Observations
+((method) @variable.builtin
+  (#any-of? @variable.builtin "GET" "HEAD" "OPTIONS" "TRACE" "CONNECT" "LIST" "WEBSOCKET"))
+
+(identifier) @variable
+
+;; YELLOW: Routing & Logic (Redirections & Script Bounds)
+[
+  ">"
+  "<"
+  "{%"
+  "%}"
+] @keyword.conditional
 
 ;; BLUE: Structure & Definition
-((comment
-  name: (_)) @keyword.declaration
-  (#set! priority 105))
+(header 
+  name: (header_entity) @tag)
 
-((comment
-  name: (_) @keyword.function)
-  (#set! priority 110))
-
-((comment
-  "=" @punctuation.delimiter)
-  (#set! priority 120))
-
-(header
-  name: (_) @tag)
-
-(variable_declaration
-  "=" @keyword.modifier)
+(request_separator) @punctuation.delimiter
 
 (variable_declaration
   "@" @keyword.modifier)
 
+(variable_declaration
+  "=" @keyword.modifier)
+
 [
   "{{"
   "}}"
-  "{%"
-  "%}"
 ] @punctuation.bracket
 
 (header
   ":" @punctuation.delimiter)
 
-">" @punctuation.delimiter
-
-(request_separator) @punctuation.delimiter
-
-(res_handler_script 
-  ">" @punctuation.delimiter)
-
-;; CYAN: Ephemeral State
-((method) @variable.builtin
-  (#any-of? @variable.builtin "GET" "HEAD" "OPTIONS" "TRACE" "CONNECT"))
-
-(identifier) @variable
-
 ;; WHITE: Generic Data
-((comment
-  value: (_) @string)
-  (#set! priority 110))
-
-(request
-  url: (_) @string)
-
+(value) @string
+(target_url) @string
 (http_version) @string
-
-(header
-  value: (_) @string)
-
-(variable_declaration
-  (value) @string)
-
-(status_code) @number
 (status_text) @string
-
-(external_body
-  path: (_) @string)
+(path) @string
+(status_code) @number
 
 ;; BLACK: Comments
-
-;; YELLOW: Routing & Logic (Redirections)
-;; Global capture ensures it catches the token regardless of AST nesting
-"<" @keyword.conditional
+(comment) @comment @spell
