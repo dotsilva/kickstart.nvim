bfd7d90d8c8d2a3aa3e971cef16ef047dd05bd69
Bruno da Silva <dotsilva@proton.me>
2026-04-18 03:10:24 -0300

    finishing hyprlang parser fine tuning

--- queries/hyprlang/highlights.scm
+++ queries/hyprlang/highlights.scm
@@ -1,4 +1,3 @@
-;; queries/hyprlang/highlights.scm
 ;; extends
 
 ;; GREEN: Triggers & Mutations
@@ -20,6 +19,7 @@
   "-"
   "+"
 ] @function.builtin
+
 
 ;; BLUE: Structure & Definition
 (keyword
@@ -62,8 +62,16 @@
 (section
   device: (device_name) @variable.parameter)
 
-;; MAGENTA: Exceptional Data
+;; MAGENTA: Exceptional Data (Axiom 3)
 (boolean) @boolean
+(mod) @constant.builtin
+;; MAGENTA: Modifier chains with spaces (Axiom 3)
+((string) @constant.builtin
+  (#match? @constant.builtin "^[ \t]*(SUPER|SHIFT|CTRL|ALT|MOD[1-5]|WIN|LOGO|CAPS|CONTROL)([ \t]+(SUPER|SHIFT|CTRL|ALT|MOD[1-5]|WIN|LOGO|CAPS|CONTROL))*[ \t]*$"))
+
+;; MAGENTA: Single keys, common cases (Axiom 3)
+((string) @constant.builtin
+  (#match? @constant.builtin "^[ \t]*([a-zA-Z0-9]|Return|RETURN|Space|SPACE|Tab|TAB|Esc|ESC|Escape|ESCAPE|Up|UP|Down|DOWN|Left|LEFT|Right|RIGHT|F[0-9]{1,2}|Home|HOME|End|END|Page_Up|PAGE_UP|Page_Down|PAGE_DOWN|Insert|INSERT|Delete|DELETE|Backspace|BACKSPACE|Print|PRINT)[ \t]*$"))
 
 ;; WHITE: Generic Data
 [
@@ -78,6 +86,21 @@
   (angle)
   (hex)
 ] @number
+
+;; MAGENTA: Modifier chains with spaces (Axiom 3)
+;; Must stay below generic (string) @string to win precedence
+((string) @constant.builtin
+  (#match? @constant.builtin "^[ \t]*(SUPER|SHIFT|CTRL|ALT|MOD[1-5]|WIN|LOGO|CAPS|CONTROL)([ \t]+(SUPER|SHIFT|CTRL|ALT|MOD[1-5]|WIN|LOGO|CAPS|CONTROL))*[ \t]*$"))
+
+;; MAGENTA: Single keys, common cases (Axiom 3)
+;; Must stay below generic (string) @string to win precedence
+((string) @constant.builtin
+  (#match? @constant.builtin "^[ \t]*([a-zA-Z0-9]|Return|RETURN|Space|SPACE|Tab|TAB|Esc|ESC|Escape|ESCAPE|Up|UP|Down|DOWN|Left|LEFT|Right|RIGHT|F[0-9]{1,2}|Home|HOME|End|END|Page_Up|PAGE_UP|Page_Down|PAGE_DOWN|Insert|INSERT|Delete|DELETE|Backspace|BACKSPACE|Print|PRINT)[ \t]*$"))
+
+;; GREEN: Dispatchers (Axiom 1)
+;; Must stay below generic (string) @string to win precedence
+((string) @function.builtin
+  (#match? @function.builtin "^[ \t]*(exec|workspace|killactive|togglegroup|pass)[ \t]*$"))
 
 ;; BLACK: Comments
 (comment) @comment @spell
