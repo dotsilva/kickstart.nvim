ceb6684886984cf7ae1bff4049ee8a54fb3707d3
Bruno da Silva <dotsilva@proton.me>
2026-04-23 20:34:45 -0300

    fine tuning markdown \* parsers

--- queries/markdown/highlights.scm
+++ queries/markdown/highlights.scm
@@ -1,45 +1,24 @@
 ;; extends
 
-;; GREEN: Triggers & Mutations
+;; GREEN: Mutations/Execution
+(fenced_code_block) @markup.raw.block
 (indented_code_block) @markup.raw.block
+(html_block) @markup.raw.block
 
-((fenced_code_block) @markup.raw.block
-  (#set! priority 90))
+;; BLUE: Structure
+(atx_heading) @markup.heading
+(setext_heading) @markup.heading
 
-(fenced_code_block
-  (fenced_code_block_delimiter) @markup.raw.block
-  (#set! conceal "")
-  (#set! conceal_lines ""))
-
-;; BLUE: Structure & Definition
-(setext_heading
-  (paragraph) @markup.heading
-  (setext_h1_underline) @markup.heading)
-
-(setext_heading
-  (paragraph) @markup.heading
-  (setext_h2_underline) @markup.heading)
-
-(atx_heading
-  (atx_h1_marker)) @markup.heading
-
-(atx_heading
-  (atx_h2_marker)) @markup.heading
-
-(atx_heading
-  (atx_h3_marker)) @markup.heading
-
-(atx_heading
-  (atx_h4_marker)) @markup.heading
-
-(atx_heading
-  (atx_h5_marker)) @markup.heading
-
-(atx_heading
-  (atx_h6_marker)) @markup.heading
-
-(pipe_table_header
-  (pipe_table_cell) @markup.heading)
+[
+  (atx_h1_marker)
+  (atx_h2_marker)
+  (atx_h3_marker)
+  (atx_h4_marker)
+  (atx_h5_marker)
+  (atx_h6_marker)
+  (setext_h1_underline)
+  (setext_h2_underline)
+] @markup.heading
 
 [
   (list_marker_plus)
@@ -47,59 +26,35 @@
   (list_marker_star)
   (list_marker_dot)
   (list_marker_parenthesis)
-  (task_list_marker_unchecked)
-  (task_list_marker_checked)
 ] @markup.list
 
-((block_quote) @markup.quote
-  (#set! priority 90))
-
-([
-  (plus_metadata)
-  (minus_metadata)
-] @keyword.function
-  (#set! priority 90))
-
+(block_quote_marker) @markup.quote
 (pipe_table_header
-  "|" @punctuation.delimiter)
+  (pipe_table_cell) @markup.heading)
 
-(pipe_table_row
-  "|" @punctuation.delimiter)
+;; BLUE: Table Pipes
+(pipe_table_header "|" @punctuation.delimiter)
+(pipe_table_row "|" @punctuation.delimiter)
+(pipe_table_delimiter_row "|" @punctuation.delimiter)
 
-(pipe_table_delimiter_row
-  "|" @punctuation.delimiter)
+(fenced_code_block_delimiter) @punctuation.delimiter
 
-(pipe_table_delimiter_cell) @punctuation.delimiter
-
-((link_label)
-  .
-  ":" @punctuation.delimiter)
-
+;; BLUE: Task Brackets (Alias to delimiter for Blue)
 [
-  (block_continuation)
-  (block_quote_marker)
-  (thematic_break)
-] @punctuation.special
+  (task_list_marker_unchecked)
+  (task_list_marker_checked)
+] @punctuation.delimiter
+
+;; CYAN: Task 'x' (Slice inner char)
+((task_list_marker_checked) @property
+  (#offset! @property 0 1 0 -1))
 
 ;; CYAN: Ephemeral State
-(info_string) @label
+(info_string (language) @label)
+(link_reference_definition
+  (link_label) @markup.link.label)
 
-(fenced_code_block
-  (info_string
-    (language) @label
-    (#set! conceal "")
-    (#set! conceal_lines "")))
+;; WHITE: Generic Data (Addresses/URLs)
+(link_reference_definition
+  (link_destination) @string)
 
-[
-  (link_title)
-  (link_label)
-] @markup.link.label
-
-;; MAGENTA: Exceptional Data
-(backslash_escape) @string.escape
-
-;; BLACK: Comments & Passive Data
-(link_destination) @markup.link.url
-
-;; WHITE: Generic Data & Spellcheck
-(inline) @spell
